a148a207933eb0649a4ee957bb2d508de2e825f1
bina1204 <akaseto@gmail.com>
2013-09-05 10:16:58 +0900

    PullToRefresh のライブラリを使用した

--- PullToRefreshSample/src/com/gsbina/android/pulltorefreshsample/MainActivity.java
+++ PullToRefreshSample/src/com/gsbina/android/pulltorefreshsample/MainActivity.java
@@ -1,22 +1,72 @@
 package com.gsbina.android.pulltorefreshsample;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.Arrays;
+import java.util.LinkedList;
 
-public class MainActivity extends Activity {
+import android.app.ListActivity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+public class MainActivity extends ListActivity {
+
+	private static final String[] INITIAL_LIST = {
+			"最初に", "表示される", "リストの", "項目で", "あります",
+	};
+
+	private PullToRefreshListView mPullToRefreshListView;
+	private LinkedList<String> mItemList;
+	private ArrayAdapter<String> mAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mPullToRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
+
+		mPullToRefreshListView
+				.setOnRefreshListener(new OnRefreshListener<ListView>() {
+
+					@Override
+					public void onRefresh(
+							PullToRefreshBase<ListView> refreshView) {
+						// TODO Auto-generated method stub
+						new GetDataTask().execute();
+
+					}
+				});
+
+		mItemList = new LinkedList<String>();
+		mItemList.addAll(Arrays.asList(INITIAL_LIST));
+		mAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, mItemList);
+		ListView actualListView = mPullToRefreshListView.getRefreshableView();
+		actualListView.setAdapter(mAdapter);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	private class GetDataTask extends AsyncTask<Void, Void, Boolean> {
 
+		@Override
+		protected Boolean doInBackground(Void... arg0) {
+			try {
+				Thread.sleep(4000);
+			} catch (InterruptedException e) {
+			}
+			return true;
+		}
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+			mItemList.addFirst("追加された項目");
+			mAdapter.notifyDataSetChanged();
+			mPullToRefreshListView.onRefreshComplete();
+			super.onPostExecute(result);
+		}
+
+	}
 }
