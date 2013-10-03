51148122710737266a446c94dd7a15a314549dbc
bina1204 <akaseto@gmail.com>
2013-10-03 11:13:01 +0900

    DrawerToggle の状態を同期する

--- NavigationDrawerSample/src/com/gsbina/android/navigationdrawersample/MainActivity.java
+++ NavigationDrawerSample/src/com/gsbina/android/navigationdrawersample/MainActivity.java
@@ -1,15 +1,38 @@
 package com.gsbina.android.navigationdrawersample;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+
+	private DrawerLayout mDrawer;
+	private ActionBarDrawerToggle mDrawerToggle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		ActionBar ab = getSupportActionBar();
+		ab.setDisplayHomeAsUpEnabled(true);
+		ab.setHomeButtonEnabled(true);
+
+		mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawer.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer,
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_close);
+
+		mDrawer.setDrawerListener(mDrawerToggle);
+
 	}
 
 	@Override
@@ -17,6 +40,28 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// DrawerToggleの状態を同期する
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// DrawerToggleの状態を同期する
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
